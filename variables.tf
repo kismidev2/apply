93e1f4e56b78ed739a4d3c4560337066447a01a9
kismidev2 <110072828+kismidev2@users.noreply.github.com>
2022-07-29 14:52:14 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -339,7 +339,7 @@ variable "kubernetes_cluster__msaenterrg" {
 
 variable "name__msaenterrg" {
     type = string
-    default = "msaenterrg"
+    default = "myyang07"
     description = "[text] resource group name"
 }
 
